8fba53ca2e7c23579478b45bd37aa89bdb0695ae
haisi.qu <haisi.qu@avatec.ai>
2019-06-24 15:33:47 +0800

    增加21. Merge Two Sorted Lists

--- src/MergeTwoSortedLists.js
+++ src/MergeTwoSortedLists.js
@@ -11,20 +11,54 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-  var basic = l2.val > l1.val ? l1 : l2;
-  var comp = l2.val > l1.val ? l2 : l1;
-  var index = comp;
-  while(index) {
-    if(basic.val > comp.val) {
-      var tempNext = comp.next;
-      comp.next = basic;
-      basic.next = tempNext;
+  if(l1 === null || l2 === null) { // 缺少空值判断
+    return l1 ? l1 : l2
+  }
+  var result = {val: null, next: null};
+  if(l1.val < l2.val) {
+    result = l1;
+    l1 = l1.next;
+  } else {
+    result = l2;
+    l2 = l2.next;
+  }
+  var index = result
+  while(l1 && l2) {
+    if(l1.val < l2.val) {
+      index.next = l1;
+      l1 = l1.next;
     } else {
-      var tempNext = basic.next;
-      basic.next = comp.next;
-      comp.next = tempNext;
+      index.next = l2;
+      l2 = l2.next;
     }
+    // console.log('result: ' + JSON.stringify(result))
+    // console.log('index ' + JSON.stringify(index))
     index = index.next;
   }
-  return basic;
+  index.next = l1 ? l1 : l2;
+  var index2 = result
+  do{
+    console.log(index2.val)
+    index2 = index2.next
+  }while(index2)
+  return result;
 };
+
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+var l1 = new ListNode(1);
+var l2 = new ListNode(2);
+var l4 = new ListNode(4);
+l1.next = l2;
+l2.next = l4;
+
+var r1 = new ListNode(1);
+var r3 = new ListNode(3);
+var r4 = new ListNode(4);
+r1.next = r3;
+r3.next = r4;
+
+mergeTwoLists(l1, r1)
