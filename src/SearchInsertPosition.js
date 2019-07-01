10da0170f7fbfc98bf738fef747548df5a3b304f
haisi.qu <haisi.qu@avatec.ai>
2019-07-01 14:21:09 +0800

    Search Insert Position

--- src/SearchInsertPosition.js
+++ src/SearchInsertPosition.js
@@ -4,5 +4,16 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-  
+  var i;
+  for(i = 0; i < nums.length; i++) {
+    if(target <= nums[i]) {
+      break;
+    }
+  }
+  return i;
 };
+
+console.log(searchInsert([1,3,5,6], 5))
+console.log(searchInsert([1,3,5,6], 2))
+console.log(searchInsert([1,3,5,6], 7))
+console.log(searchInsert([1,3,5,6], 0))
