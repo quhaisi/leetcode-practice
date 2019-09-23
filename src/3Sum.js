3894c5f0787fb870ddbef10a24671ab887b44de6
曲海思 <qu03265610@163.com>
2019-09-23 21:32:41 +0800

    参考答案完成算法

--- src/3Sum.js
+++ src/3Sum.js
@@ -3,16 +3,13 @@
  * @return {number[][]}
  */
 
-var threeSum = function(nums) {
+var threeSum_Time_Limit_Exceeded = function(nums) {
   var matchMap = new Map()
   var result = []
   for(var i = 0; i < nums.length; i++) {
-    console.log('i', i)
     for(var j = i+1; j < nums.length; j++) {
-      console.log(nums[j])
-      console.log((matchMap.get(nums[j])||[]).includes(nums[i]))
       if((matchMap.get(nums[j])||[]).includes(nums[i])) {
-        break;
+        continue;
       }
       for(var a = j+1; a < nums.length; a++) {
         if(nums[i]+nums[j]+nums[a] == 0) {
@@ -25,7 +22,36 @@ var threeSum = function(nums) {
       }
     }
   }
-  console.log(result)
+  return result;
 };
+
+var threeSum = function(nums) {
+  var arr = [];
+  nums.sort((a, b) => {
+    return a - b
+  })
+  for(var i = 0; i < nums.length; i++) {
+    while(nums[i] === nums[i-1]) i++
+
+    let l = i + 1
+    let r = nums.length - 1
+    while(l < r) {
+      if(nums[l]+nums[r]+nums[i] === 0) {
+        arr.push([nums[l], nums[r], nums[i]])
+        l++;
+        r--;
+        while(nums[l] === nums[l-1]) l++
+        while(nums[r] === nums[r+1]) r--
+      } else if(nums[l]+nums[r]+nums[i] > 0) {
+        r--
+        while(nums[r] === nums[r+1]) r--
+      } else {
+        l++;
+        while(nums[l] === nums[l-1]) l++
+      }
+    }
+  }
+  return arr
+}
 
 threeSum([-1, 0, 1, 2, -1, -4])
