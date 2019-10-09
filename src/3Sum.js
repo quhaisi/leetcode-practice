561c55297042429cbfc2c8553656bab7be800b8c
haisi.qu <haisi.qu@avatec.ai>
2019-10-09 16:16:35 +0800

    resovle 3Sum

--- src/3Sum.js
+++ src/3Sum.js
@@ -3,7 +3,7 @@
  * @return {number[][]}
  */
 
-var threeSum = function(nums) {
+var threeSum_Time_Limit_Exceeded = function(nums) {
   var matchMap = new Map()
   var result = []
   for(var i = 0; i < nums.length; i++) {
@@ -28,4 +28,34 @@ var threeSum = function(nums) {
   console.log(result)
 };
 
-threeSum([-1, 0, 1, 2, -1, -4])
+var threeSum = function(nums) {
+  var arr = [];
+  nums.sort((a, b) => {
+    return a - b
+  })
+  for(var i = 0; i < nums.length; i++) {
+    while(nums[i] === nums[i-1]) i++
+
+    let l = i + 1
+    let r = nums.length - 1
+    while(l < r) {
+      if(nums[l]+nums[r]+nums[i] === 0) {
+        arr.push([nums[l], nums[r], nums[i]])
+        l++;
+        r--;
+        while(nums[l] === nums[l-1]) l++
+        while(nums[r] === nums[r+1]) r--
+      } else if(nums[l]+nums[r]+nums[i] > 0) {
+        r--
+        while(nums[r] === nums[r+1]) r--
+      } else {
+        l++;
+        while(nums[l] === nums[l-1]) l++
+      }
+    }
+  }
+  console.log(arr)
+  return arr
+}
+
+module.exports = threeSum;
