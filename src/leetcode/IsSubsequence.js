da0def3521e8b662ff67612f5333a4ee3b347ac4
haisi.qu <haisi.qu@avatec.ai>
2020-07-27 18:23:39 +0800

    Is Subsequence

---
+++ src/leetcode/IsSubsequence.js
@@ -0,0 +1,20 @@
+// Is Subsequence
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function (s, t) {
+  let sIndex = 0;
+  let tIndex = 0;
+  while (sIndex < s.length && tIndex < t.length) {
+    if (s[sIndex] === t[tIndex]) {
+      sIndex++;
+    }
+    tIndex++;
+  }
+  return sIndex === s.length;
+};
+
+console.log(isSubsequence("abc", "ahcbgd"));
